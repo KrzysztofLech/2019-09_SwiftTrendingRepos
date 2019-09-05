fe775c71488d0e16f302dc8696d0bb8aff898aa5
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-09-05 20:17:31 +0200

    Project files and folders structure added. StartVC added.

---
+++ SwiftTrendingRepos/View Controllers/Start/StartViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StartViewController.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 05/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+}
