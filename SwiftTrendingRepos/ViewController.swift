41523031f03722d3403bb39d60a78e1591b6692b
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-09-05 19:25:52 +0200

    Initial commit

---
+++ SwiftTrendingRepos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 05/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
