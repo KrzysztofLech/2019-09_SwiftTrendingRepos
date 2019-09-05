38f789b86e364d0b2b53b3462f7260b882b4d412
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-09-05 20:28:31 +0200

    GithubRepo model added

---
+++ SwiftTrendingRepos/Models/GithubRepo.swift
@@ -0,0 +1,30 @@
+//
+//  GithubRepo.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 05/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import Foundation
+
+struct GithubRepo: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case author
+        case name
+        case avatarUrl = "avatar"
+        case repoUrl = "url"
+        case description
+        case stars
+        case forks
+    }
+    
+    let author: String
+    let name: String
+    let avatarUrl: String
+    let repoUrl: String
+    let description: String
+    let stars: Int
+    let forks: Int
+}
