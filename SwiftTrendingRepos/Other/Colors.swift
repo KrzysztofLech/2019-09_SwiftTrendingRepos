80433ab4ccab6dba19080c38a380d19eb0c17d99
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-09-07 13:37:12 +0200

    Colors, TableLostViewController added

---
+++ SwiftTrendingRepos/Other/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 07/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let background = UIColor(named: "Background")
+    static let textLight = UIColor(named: "Text - light")
+}
---
+++ SwiftTrendingRepos/View Controllers/Table List/TableListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableListViewController.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 07/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class TableListViewController: UIViewController {
+
+}
