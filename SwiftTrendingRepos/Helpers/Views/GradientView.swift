53906b12dadb72bf10e95f47ef3a326720b8dc3f
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-09-11 21:42:35 +0200

    Presenting webView added

---
+++ SwiftTrendingRepos/Helpers/Views/GradientView.swift
@@ -0,0 +1,64 @@
+//
+//  GradientView.swift
+//  SwiftTrendingRepos
+//
+//  Created by Krzysztof Lech on 10/09/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+enum GradientSet: String {
+    case background
+    case objectBackground
+    
+    var startColor: CGColor {
+        switch self {
+        //case .background:         return UIColor(red: 54/255, green: 54/255, blue: 70/255, alpha: 1.0).cgColor
+        //case .objectBackground:   return UIColor(red: 79/255, green: 79/255, blue: 97/255, alpha: 1.0).cgColor
+            
+        case .background:         return UIColor(red: 0/255, green: 0/255, blue: 100/255, alpha: 1.0).cgColor
+        case .objectBackground:   return UIColor(red: 0/255, green: 0/255, blue: 140/255, alpha: 1.0).cgColor
+        }
+    }
+    
+    var endColor: CGColor {
+        switch self {
+        //case .background:          return UIColor(red: 31/255, green: 31/255, blue: 42/255, alpha: 1.0).cgColor
+        //case .objectBackground:    return UIColor(red: 34/255, green: 34/255, blue: 44/255, alpha: 1.0).cgColor
+            
+        case .background:          return UIColor(red: 0/255, green: 0/255, blue: 60/255, alpha: 1.0).cgColor
+        case .objectBackground:    return UIColor(red: 0/255, green: 0/255, blue: 80/255, alpha: 1.0).cgColor
+        }
+    }
+}
+
+
+import UIKit
+
+class GradientView: UIView {
+    
+    @IBInspectable var colorSet: String = ""
+    
+    override func draw(_ rect: CGRect) {
+        guard
+            let context = UIGraphicsGetCurrentContext(),
+            let gradientSet = GradientSet(rawValue: colorSet)
+            else { return }
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colors = [gradientSet.startColor, gradientSet.endColor]
+        let colorLocations: [CGFloat] = [0.0, 1.0]
+        guard
+            let gradient = CGGradient(colorsSpace: colorSpace,
+                                      colors: colors as CFArray,
+                                      locations: colorLocations)
+            else { return }
+        
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: bounds.width, y: bounds.height)
+        
+        context.drawLinearGradient(gradient,
+                                   start: startPoint,
+                                   end: endPoint,
+                                   options: [])
+    }
+}
--- SwiftTrendingRepos/View Controllers/Details/DetailsViewController.swift
+++ SwiftTrendingRepos/View Controllers/Details/DetailsViewController.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2019 Krzysztof Lech. All rights reserved.
 //
 
-import UIKit
+//import UIKit
+import WebKit
 
 class DetailsViewController: UIViewController {
     
-    @IBOutlet private var containerView: UIView!
+    @IBOutlet private var repoDetailsView: UIView!
     
     @IBOutlet private var avatarImageView: UIImageView!
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
@@ -23,14 +24,43 @@ class DetailsViewController: UIViewController {
     @IBOutlet private var starsLabel: UILabel!
     @IBOutlet private var forksLabel: UILabel!
     
+    @IBOutlet private var webViewContainerView: UIView!
+    @IBOutlet private var webView: WKWebView!
+    @IBOutlet private var webViewContainerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var webViewContainerBottomConstrait: NSLayoutConstraint!
+    
     var repoItem: GithubRepoViewModel?
     private var imageService: ImageServiceProtocol?
+    
+    private var isWebViewPresented = false
+    private lazy var compactWebViewHeight: CGFloat = {
+        return UIScreen.main.bounds.height - repoDetailsView.frame.origin.y - repoDetailsView.bounds.height - repoDetailsView.frame.origin.x
+    }()
+    private lazy var maximumWebViewHeight: CGFloat = {
+        return UIScreen.main.bounds.height - repoDetailsView.frame.origin.y + 22
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imageService = ImageService()
+        webViewContainerView.alpha = 0.0
         presentData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupWebView()
+    }
+    
+    private func setupWebView() {
+        webView.navigationDelegate = self
+        webViewContainerHeightConstraint.constant = compactWebViewHeight
+        webViewContainerBottomConstrait.constant = -compactWebViewHeight
+        
+        guard let urlString = repoItem?.repoUrl, let url = URL(string: urlString) else { return }
+        webView.load(URLRequest(url: url))
     }
     
     private func presentData() {
@@ -61,5 +91,38 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func repoUrlButtonAction() {
+        showWebView()
+    }
+    
+    @IBAction func webViewButtonAction(sender: UIButton) {
+        isWebViewPresented.toggle()
+        handleFullScreenWebView()
+    }
+    
+    private func showWebView() {
+        webViewContainerBottomConstrait.constant = 0
+        webViewContainerView.alpha = 1.0
+        animateView(withDuration: 1.0)
+    }
+    
+    private func handleFullScreenWebView() {
+        webViewContainerHeightConstraint.constant = isWebViewPresented ? maximumWebViewHeight : compactWebViewHeight
+        animateView(withDuration: 0.3)
+    }
+    
+    private func animateView(withDuration duration: Double) {
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveEaseOut, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+}
+
+extension DetailsViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        // TODO: error handling
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        showWebView()
     }
 }
